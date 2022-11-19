49d789b1bf4a7f79fca1bff76a74a234877a73c3
PresentJay <presentj94@gmail.com>
2022-11-20 02:06:37 +0900

    feat: test coverage에서 generated pattern 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HealthModule, UsersModule, PrismaModule],
+  imports: [
+    HealthModule,
+    UsersModule,
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
